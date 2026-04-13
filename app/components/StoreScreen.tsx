4807d1acb66c9b7e4832a149b215b45d68db3e6c
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-04-13 11:24:48 +0700

    Update code

--- app/components/StoreScreen.tsx
+++ app/components/StoreScreen.tsx
@@ -1,11 +1,32 @@
 import Link from "next/link";
+import { Playfair_Display } from "next/font/google";
+
+const playfairDisplayBold = Playfair_Display({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+const playfairDisplayRegular = Playfair_Display({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const StoreScreen = () => {
   return (
-    <div>
+    <section className="py-10">
+      <div className="max-w-7xl mx-auto px-6">
+        <div className="flex items-center justify-between mb-10">
+          <h1
+            className={`text-3xl md:text-4xl text-center md:text-left font-bold ${playfairDisplayBold.className}`}
+          >
+            Store
+          </h1>
+        </div>
 
-    </div>
-  )
-}
+        <div></div>
+      </div>
+    </section>
+  );
+};
 
 export default StoreScreen;
