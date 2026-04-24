d63c611ef4d573eb691887710c5b8817cf371384
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-04-24 12:55:54 +0700

    Update code

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import NavbarClient from "../components/client/NavbarClient";
 import Image from "next/image";
 import { Playfair_Display } from "next/font/google";
@@ -21,134 +22,20 @@ interface Product {
 }
 
 async function getProducts(): Promise<Product[]> {
-  return [
-    {
-      id: 1,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 2,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 3,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 4,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 5,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 6,
-      title: "O2H Hostle Navy Half-Zip Jacket",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket.jpg",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 7,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 8,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 9,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 10,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 11,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 12,
-      title: "O2H NFL Black Jersey",
-      price: "Rp. 465.000",
-      image: "/img/products/O2H_NFL_Black_Jersey.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 13,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 14,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 15,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 16,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 17,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-    {
-      id: 18,
-      title: "O2H Hostel Embroidery Baseball Jersey Black",
-      price: "Rp. 550.000",
-      image: "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black.png",
-      sold: "10RB+ Terjual",
-    },
-  ];
+  const headersList = await headers();
+  const host = headersList.get("host") || "localhost:3000";
+
+  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+  const res = await fetch(`http://${host}/api/products`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return res.json();
 }
 
 export default async function SearchPage({
