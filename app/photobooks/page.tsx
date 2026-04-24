7d79ad45a011a144714f958fe8b4602bd06fd4be
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-04-24 12:47:53 +0700

    Update code

--- app/photobooks/page.tsx
+++ app/photobooks/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/client/NavbarClient";
+import FloatingLogo from "../components/client/FloatingLogo";
 
 export default function Photobooks() {
   return (
     <>
       <title>Photobooks | O2H Website Center</title>
       <Navbar />
+
+      <FloatingLogo />
     </>
   )
 };
--- app/stores/page.tsx
+++ app/stores/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/client/NavbarClient";
+import FloatingLogo from "../components/client/FloatingLogo";
 
 export default function Stores() {
   return (
     <>
       <title>Store | O2H Website Center</title>
       <Navbar />
+
+      <FloatingLogo />
     </>
   );
 };
